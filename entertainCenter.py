9cb39e9480b2e0caee6f4e986c8425e67468deb1
Unknown <294188719@qq.com>
2017-06-04 16:25:36 +1000

    Some documentation
    some comments

--- entertainCenter.py
+++ entertainCenter.py
@@ -8,16 +8,20 @@ Created on Sun Jun  4 11:01:15 2017
 import fresh_tomatoes
 import movie
 
-movies = []
 
+movies = [] #list of movie
+
+#the movie wonder women
 wonderWomen = movie.Movie("Wonder Women",
                     "http://cdn2-www.comingsoon.net/assets/uploads/gallery/wonder-woman/14883647_561931980643670_384850897829389102_o.jpg",
                     "https://youtu.be/1Q8fG0TtVAY")
 
+#the movie justice league
 justiceLeague = movie.Movie("Justice League",
                       "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
                       "https://youtu.be/3cxixDgHUYw")
 
+#add movies to the list
 movies.append(wonderWomen)
 movies.append(justiceLeague)
 
--- movie.py
+++ movie.py
@@ -6,7 +6,7 @@ Created on Sun Jun  4 10:28:01 2017
 @author: lixiaochi
 """
 class Movie:
-    """"""
+    """The movie data contains a movie title, post image url and trailer url"""
     def __init__(self, movieName,postUrl,trailerUrl):
         self.title =  movieName
         self.poster_image_url = postUrl
