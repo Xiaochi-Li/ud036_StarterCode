fe8a73c75f73900e6bee23316e66a379e58f3533
Unknown <294188719@qq.com>
2017-06-06 19:03:53 +1000

    reformate code
    Reformate code so that they follows PEP8 rules

--- entertainCenter.py
+++ entertainCenter.py
@@ -9,19 +9,22 @@ import fresh_tomatoes
 import movie
 
 
-movies = [] #list of movie
+movies = []  # list of movie
 
-#the movie wonder women
+# the movie wonder women
 wonderWomen = movie.Movie("Wonder Women",
-                    "http://cdn2-www.comingsoon.net/assets/uploads/gallery/wonder-woman/14883647_561931980643670_384850897829389102_o.jpg",
-                    "https://youtu.be/1Q8fG0TtVAY")
+                          'http://cdn2-www.comingsoon.net/assets/'
+                          'uploads/gallery/wonder-woman/14883647_5619'
+                          '31980643670_384850897829389102_o.jpg',
+                          "https://youtu.be/1Q8fG0TtVAY")
 
-#the movie justice league
+# the movie justice league
 justiceLeague = movie.Movie("Justice League",
-                      "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
-                      "https://youtu.be/3cxixDgHUYw")
+                            'https://upload.wikimedia.org/wikipedia/'
+                            'en/3/31/Justice_League_film_poster.jpg',
+                            "https://youtu.be/3cxixDgHUYw")
 
-#add movies to the list
+# add movies to the list
 movies.append(wonderWomen)
 movies.append(justiceLeague)
 
--- movie.py
+++ movie.py
@@ -5,11 +5,11 @@ Created on Sun Jun  4 10:28:01 2017
 
 @author: lixiaochi
 """
+
+
 class Movie:
     """The movie data contains a movie title, post image url and trailer url"""
-    def __init__(self, movieName,postUrl,trailerUrl):
-        self.title =  movieName
+    def __init__(self, movieName, postUrl, trailerUrl):
+        self.title = movieName
         self.poster_image_url = postUrl
         self.trailer_youtube_url = trailerUrl
-        
-
